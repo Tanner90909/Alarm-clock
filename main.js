e9d53fdc40a932f295bda47b40ea4cbe0d5a8a71
Tanner McDaniel <tmmcdaniel909@gmail.com>
2023-09-14 12:55:31 +0000

    got the time ticking on webpage

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let isAlarmActive = false;
 // create a function to update the current time and display it
 function updateClockTime () {
     const currentTimeElement = document.getElementById("current-time");
+    const currentTime = new Date();
     const hours = currentTime.getHours().toString().padStart(2, "0");
     const minutes = currentTime.getMinutes().toString().padStart(2, "0");
     const seconds = currentTime.getSeconds().toString().padStart(2, "0");
